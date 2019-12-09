e5927e0c73f8d134265e042c6c2579eba4575414
Shou <nmmarzano@users.noreply.github.com>
2019-12-09 14:03:37 -0300

    Update part_one.py

--- day-4/part_one.py
+++ day-4/part_one.py
@@ -1,7 +1,32 @@
 import math
 
+# eliminated some, real input was 168630-718098
 input_start = 168888
 input_end = 699999
+
+
+# test function
+def has_same_digits_adjacent(n):
+    prev = n % 10
+    while n > 1:
+        n = math.floor(n / 10)
+        cur = n % 10
+        if prev == cur:
+            return True
+        prev = cur
+    return False
+
+
+# test function
+def is_never_decreasing(n):
+    prev = n % 10
+    while n > 1:
+        n = math.floor(n / 10)
+        cur = n % 10
+        if prev < cur:
+            return False
+        prev = cur
+    return True
 
 
 def is_valid(n):
@@ -27,7 +52,6 @@ def valid_combinations(start, end):
 
 
 def main():
-    print('from {0} to {1}'.format(input_start,input_end))
     print(valid_combinations(input_start, input_end))
 
 
